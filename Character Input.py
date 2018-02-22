7b458ccf51405ea35660f1c040bb787de92b63ea
Alessandro Di Micoli <adimicoli@gmail.com>
2018-02-22 00:11:54 +0000

    added variable year

--- Character Input.py
+++ Character Input.py
@@ -3,3 +3,13 @@ print("Your name is " + name)
 
 age = int(input("Enter your age: "))
 print("Your age is " + str(age))
+
+year = str((2014 - age)+100)
+print(name + " will be 100 years old in the year " + year)
+
+print("Were " + "wolf")
+print("Door " + "man")
+print("4 " + "chan")
+print(str(4) + " chan")
+
+print(4 * "test ")
