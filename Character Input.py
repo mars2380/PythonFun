dcfdd22d83bb9affa754f6b3863b3d1efeeb0d89
Alessandro Di Micoli <adimicoli@gmail.com>
2018-02-22 15:12:51 +0000

    added vcs.xml

--- Character Input.py
+++ Character Input.py
@@ -1,5 +1,5 @@
-name = input("Give me your name: ")
-print("Your name is " + name)
+#name = input("Give me your name: ")
+# print("Your name is " + name)
 
 age = int(input("Enter your age: "))
 print("Your age is " + str(age))
